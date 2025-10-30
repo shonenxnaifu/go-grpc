6d158c0b70427e59641a595cc4463bc25520c179
Pawitra Warda <pawitrawarda@gmail.com>
2025-10-30 16:00:04 +0700

    strings and runes

---
+++ 2-intermediate/strings_runes.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	message := "Hello, \nGo!"
+	message1 := "Hello, \tGo!"
+	message2 := "Hello, Go!"
+	rawMessage := `Hello\nGo` // string literal
+
+	fmt.Println(message)
+	fmt.Println(message1)
+	fmt.Println(message2)
+	fmt.Println(rawMessage)
+
+	fmt.Println("length of message variable is: ", len(message))
+	fmt.Println("length of message2 variable is: ", len(message2))
+	fmt.Println("length of message2 variable is: ", len(rawMessage))
+
+	fmt.Println("the first character in message var is: ", message[0]) // ASCII
+
+	greeting := "Hello "
+	name := "Alice"
+	fmt.Println(greeting + name)
+
+	str1 := "Apple"  // A has an ASCII value of 65
+	str := "apple"   // A has an ASCII value of 97
+	str2 := "Banana" // b has an ASCII value of 98
+	str3 := "app"    // a has an ASCII value of 97
+	fmt.Println(str1 < str2)
+	fmt.Println(str3 < str1)
+	fmt.Println(str > str1)
+	fmt.Println(str > str3)
+
+	for _, char := range message {
+		// fmt.Printf("Character at index %d is %c\n", i, char)
+		fmt.Printf("hex: %x\n", char) // hexadecimal value
+		fmt.Printf("dec: %v\n", char) // ASCII or uint8  value
+
+	}
+
+	fmt.Println("Rune count: ", utf8.RuneCountInString(greeting))
+
+	greetingWithName := greeting + name
+	fmt.Println(greetingWithName)
+
+	var ch rune = 'a'
+	jch := '日'
+	fmt.Println(ch)
+	fmt.Println(jch)
+
+	fmt.Printf("%c\n", ch)
+	fmt.Printf("%c\n", jch)
+
+	cstr := string(ch)
+	fmt.Println(cstr)
+	fmt.Printf("Type of cstr is %T\n", cstr)
+
+	const NIHONGO = "日本の保育園"
+	fmt.Println(NIHONGO)
+
+	jhello := "こんにちは"
+	for _, runeValue := range jhello {
+		fmt.Printf("%c - %v\n", runeValue, runeValue)
+	}
+
+	r := '😊'
+	fmt.Printf("%v\n", r)
+	fmt.Printf("%c\n", r)
+}
